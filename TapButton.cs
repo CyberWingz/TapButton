d1f51710b36126a14a85a56ea821a67c3ba08958
CyberWingz <wen_bin89@hotmail.com>
2016-04-24 21:45:24 +0800

    Hello World

---
+++ TapButton.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class TapButton : Graphic, IPointerDownHandler, IPointerUpHandler, IPointerEnterHandler, IPointerExitHandler {
+
+
+	readonly byte flagIsDown = 0x01;
+	readonly byte flagIsExit = 0x02;
+
+	byte myFlag = 0x0;
+
+	[SerializeField] TapButtonAnim animShrink;
+	[SerializeField] TapButtonAnim animBounce;
+	[SerializeField] Image image;
+
+	Coroutine coro;
+	RectTransform _rectT;
+
+
+
+	RectTransform rectT
+	{
+		get
+		{
+			if(_rectT == null)
+			{
+				if(image != null)
+					_rectT = image.rectTransform;
+				else
+					_rectT = rectTransform;
+			}
+			
+			return _rectT;
+		}
+	}
+
+	protected override void OnPopulateMesh(VertexHelper vh)
+	{
+		vh.Clear();
+	}
+
+	public void OnPointerUp(PointerEventData eventData)
+	{
+		print("OnPointerUp");
+
+		if(eventData.dragging == true || (myFlag & flagIsExit) != 0)
+		{
+			rectT.localScale = Vector3.one;
+		}
+		else
+		{
+			StartCoroutine(animBounce.iScale(rectT));
+		}
+		myFlag = 0;
+	}
+
+	public void OnPointerDown(PointerEventData eventData)
+	{
+		print("OnPointerDown");
+
+		StartCoroutine(animShrink.iScale(rectT));
+		ApplyFlag(ref myFlag, flagIsDown);
+	}
+
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+		print("OnPointerEnter");
+
+		if(CheckFlag(myFlag, flagIsDown))
+		{
+			StartCoroutine(animShrink.iScale(rectT));
+			RemoveFlag(ref myFlag, flagIsExit);
+		}
+	}
+
+	public void OnPointerExit(PointerEventData eventData)
+	{
+		print("OnPointerExit");
+
+		if(CheckFlag(myFlag, flagIsDown))
+		{
+			rectT.localScale = Vector3.one;
+			ApplyFlag(ref myFlag, flagIsExit);
+		}
+	}
+
+	bool CheckFlag(byte a, byte b)
+	{
+		print((a & b) != 0);
+		return (a & b) != 0;
+	}
+
+	void ApplyFlag(ref byte a, byte b)
+	{
+		a |= b;
+	}
+
+	void RemoveFlag(ref byte a, byte b)
+	{
+		a &= (byte)~b;
+	}
+
+}
---
+++ TapButtonAnim.cs
@@ -0,0 +1,96 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+using System.Collections;
+using UnityEngine;
+
+public class TapButtonAnim : ScriptableObject
+{
+	#if UNITY_EDITOR
+	[MenuItem("Assets/Create/TapButtonData")]
+	public static void CreateAsset ()
+	{
+		ScriptableObjectUtility.CreateAsset<TapButtonAnim> ();
+	}
+	#endif
+
+	public AnimationCurve curve;
+	public float duration;
+
+
+	public IEnumerator iPosY(RectTransform rectT)
+	{
+		float t = 0;
+		float val = curve.Evaluate(0);
+		Vector2 pos = new Vector2(0, val);
+
+		do
+		{
+			rectT.anchoredPosition = pos;
+			yield return null;
+			t += Time.deltaTime;
+			pos.y = curve.Evaluate(t);
+
+		} while (t <= duration);
+
+		pos.y = curve.Evaluate(duration);
+		rectT.anchoredPosition = pos;
+	}
+
+	public IEnumerator iScaleY(Transform transform)
+	{
+		float t = 0;
+
+		Vector3 scale = transform.localScale;
+		scale.y = curve.Evaluate(0);
+
+		do
+		{
+			transform.localScale = scale;
+			yield return null;
+			t += Time.deltaTime;
+			scale.y = curve.Evaluate(t);
+
+		} while (t <= duration);
+
+		scale.y = curve.Evaluate(duration);
+		transform.localScale = scale;
+	}
+
+	public IEnumerator iScale(Transform transform)
+	{
+		float t = 0;
+		float val = curve.Evaluate(0);
+		Vector3 scale = new Vector3(val, val, 0);
+
+		do
+		{
+			transform.localScale = scale;
+			yield return null;
+			t += Time.deltaTime;
+			scale.x = scale.y = curve.Evaluate(t);
+
+		} while (t <= duration);
+
+		scale.x = scale.y = curve.Evaluate(duration);
+		transform.localScale = scale;
+	}
+
+	public IEnumerator iAlpha(CanvasGroup canvasGrp)
+	{
+		float t = 0;
+		float val = curve.Evaluate(0);
+
+		do
+		{
+			canvasGrp.alpha = val;
+			yield return null;
+			t += Time.deltaTime;
+			val = curve.Evaluate(t);
+
+		} while (t <= duration);
+
+		canvasGrp.alpha = curve.Evaluate(duration);
+	}
+}
